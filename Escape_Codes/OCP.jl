191ab088108e047338719d862963aec9012ad710
sandeepprakash6666 <sandeepprakash6666@gmail.com>
2020-09-08 17:23:43 +0200

    Basic TES Model added

--- Escape_Codes/OCP.jl
+++ Escape_Codes/OCP.jl
@@ -1,8 +1,3 @@
-#=
-28/8/20 - Exercise 5 from MPC modeule 2019 version at NTNU.
-Creates collocation matrix of 3rd order Radau polynomial
-Solves OCP taking in initiial differential state, previous MV (for penalizing input moves) and setpoint trajectory
-=#
 
 using Plots
 using JuMP
@@ -29,54 +24,55 @@ function Collocation_Matrix()
       return M
 end
 
-function Solve_OCP(x0, u₋₁, y_sp)
+# function Solve_OCP(x0, Tf, Is_ISS)
 
             #region -> Value of Arguments for Debugging
-            # x0 = [1.0; 1.0]
-            # u₋₁ = 0.3
-            # y_sp = append!(1.5302*ones(10), 0.9951*ones(10) )
+            x0 = [60.0]
+            Tf = 30.0
+            Is_SS = 0          #If want to find Steady State set = 1
 
             #endregion
 
-      ##*Model Parameters 
-      x2_f = 4
-      km = 0.12
-      k1 = 0.4545
-      Y = 0.4
-      μ_max = 0.53
-      D_min = 0
-      D_max = 1
-
-      ##*OCP Parameters
-      T0 = 0.0
-      Tf = 30.0
-      NFE = 30
-      dt = (Tf - T0) / NFE
-      NCP = 3
-
                   #region -> Setting Initial guesses and Dimensions
-                  q0 = 0
-                  z0 = [0.0]
-                  u0 = [0.0]
+                  z0 = [60.0; 60.0; 60.0]
+                  u0 = [0.0; 0.0]
 
-                  dx0 = 0 * x0
+                  dx0 = 0  * x0
                   alg0 = 0 * z0
-                  dq0 = 0
                   Nx = size(x0, 1)
                   Nz = size(z0, 1)
                   Nu = size(u0, 1)
                   #endregion
 
-      ##* Defining Solver
+      ##*Model Parameters 
+      ρ_dh = 1000
+      Cp_dh = 4.18
+      q_dh = 1
+
+      ρ_wh = 1000
+      Cp_wh = 4.18
+      q_wh = 1
+
+      T_dh_ret = 60
+      T_dh_minSup = 60
+
+      Q_whb = 0
+      V_tes = 500
+
+      ##*OCP Parameters
+      T0 = 0.0
+      NFE = 30
+      dt = (Tf - T0) / NFE
+      NCP = 3
+
+
+##* Defining Solver
       model1 = Model(with_optimizer(Ipopt.Optimizer))
 
             #region -> Variables and Objective
             ## Declare Variables
             @variable(model1, x[1:Nx, 1:NFE, 1:NCP])
             @variable(model1, dx[1:Nx, 1:NFE, 1:NCP])
-
-            @variable(model1, q[1, 1:NFE, 1:NCP])
-            @variable(model1, dq[1, 1:NFE, 1:NCP])
 
             @variable(model1, z[1:Nz, 1:NFE, 1:NCP])
             @variable(model1, alg[1:Nz, 1:NFE, 1:NCP])
@@ -86,7 +82,7 @@ function Solve_OCP(x0, u₋₁, y_sp)
                   #region -> Set Variable Bounds AND Initial Guesses 
                   for nx in 1:Nx, nz in 1:Nz, nu in 1:Nu, nfe in 1:NFE, ncp in 1:NCP     
                         set_lower_bound(x[nx, nfe, ncp], 0)
-                        set_upper_bound(x[1, nfe, ncp], 4.5)
+                        # set_upper_bound(x[nx, nfe, ncp], 999)
 
                         set_lower_bound(z[nz, nfe, ncp], 0)
                         #set_upper_bound(z[nz, nfe, ncp], 999)
@@ -94,85 +90,84 @@ function Solve_OCP(x0, u₋₁, y_sp)
                         #set_lower_bound(dx[nx, nfe, ncp], 0)
                         #set_upper_bound(dx[nx, nfe, ncp], 999)
 
-                        #set_lower_bound(alg_coll[nz, nfe, ncp], 0)
-                        #set_upper_bound(alg_coll[nz, nfe, ncp], 999)
+                        #set_lower_bound(alg[nz, nfe, ncp], 0)
+                        #set_upper_bound(alg[nz, nfe, ncp], 999)
 
-                        set_lower_bound(u[nu, nfe], D_min)
-                        set_upper_bound(u[nu, nfe], D_max)
+                        set_lower_bound(u[1, nfe], 0)
+                        set_upper_bound(u[1, nfe], 1)
+                        set_lower_bound(u[2, nfe], 0)
+                        # set_upper_bound(u[2, nfe], 0)                       
                   end
 
                   #Set Initial Guesses
                   for nx in 1:Nx, nz in 1:Nz, nu in 1:Nu, nfe in 1:NFE, ncp in 1:NCP
-                        set_start_value(x[nx, nfe, ncp], x0[nx])
-                        set_start_value(z[nz, nfe, ncp], z0[nz])
-                        set_start_value(dx[nx, nfe, ncp], dx0[nx])
-                        set_start_value(alg[nz, nfe, ncp], alg0[nz])
-                        set_start_value(u[nu, nfe], u0[nu])
-                        set_start_value(q[1, nfe, ncp], q0)
-                        set_start_value(dq[1, nfe, ncp], dq0)
+                        set_start_value(x[nx, nfe, ncp],    x0[nx])
+                        set_start_value(z[nz, nfe, ncp],    z0[nz])
+                        set_start_value(dx[nx, nfe, ncp],   dx0[nx])
+                        set_start_value(alg[nz, nfe, ncp],  alg0[nz])
+                        set_start_value(u[nu, nfe],         u0[nu])
+
 
                   #endregion
                   end
 
+                  #region-> Expressions for Unscaling Variables
+                  # @NLexpressions(model1, begin
+                  
+                        # Z1[nfe in 1:NFE, ncp in 1:NCP], 2.0*z[1, nfe, ncp]
+
+                  # end)
+                  #endregion
+
+
             ## Objective
-            @NLobjective(model1, Min, q[1, NFE, NCP])
-            
+            @NLobjective(model1, Min, sum( u[2,nfe] for nfe in 1:NFE ) )
+
             #endregion
 
-
-      #region -> Constraints
+##* Defining Constraints
+     #region -> Constraints
 
       @NLconstraints(model1, begin
             #Defining the model ODEs in each line
-            Constr_ODE1[nfe in 1:NFE, ncp in 1:NCP], dx[1, nfe, ncp]      == x[1, nfe, ncp] * (z[1, nfe, ncp] - u[1, nfe])
-            Constr_ODE2[nfe in 1:NFE, ncp in 1:NCP], dx[2, nfe, ncp]      == u[1, nfe]      * (x2_f - x[2, nfe, ncp])           - z[1, nfe, ncp] * x[1, nfe, ncp] / Y
+            Constr_ODE1[nfe in 1:NFE, ncp in 1:NCP], dx[1, nfe, ncp]      == u[1, nfe]*q_dh*(z[3,nfe,ncp] - x[1,nfe,ncp] )/V_tes
             #In case of more states - pattern
             #Constr_ODE999[nfe=1:NFE, ncp=1:NCP], dx[999,nfe,ncp] ==
       end)
 
       @NLconstraints(model1, begin
-            #Collocation for Quadrature/ Objective Function
-            #t = 0
-            Constr_quad_dot0[nfe = 1    , ncp in 1:NCP], dq[1, nfe, ncp]  == (x[1,nfe,ncp] - y_sp[nfe])^2 + 0.5*(u[1,nfe]  - u₋₁[1]    )^2
-            #t = 1..N
-            Constr_quad_dot[nfe in 2:NFE, ncp in 1:NCP], dq[1, nfe, ncp]  == (x[1,nfe,ncp] - y_sp[nfe])^2 + 0.5*(u[1,nfe] - u[1,nfe-1])^2
-            #In case of objective depends on end state only -> dq = dx[2] etc
-      end)
-
-
-      @NLconstraints(model1, begin
             #Defining Model Algebraic Equations in each line
-            Constr_Alg1[nfe in 1:NFE, ncp in 1:NCP], z[1, nfe, ncp] == (  μ_max * x[2, nfe, ncp] / (km + x[2, nfe, ncp] + k1 * x[2, nfe, ncp]^2) )
+            Constr_Alg1[nfe in 1:NFE, ncp in 1:NCP], z[1, nfe, ncp] == u[1,nfe]*x[1,nfe,ncp]    + (1-u[1,nfe])*z[3,nfe,ncp]
+            Constr_Alg2[nfe in 1:NFE, ncp in 1:NCP], z[2, nfe, ncp] == z[1,nfe,ncp]             + u[2,nfe]/( q_dh*ρ_dh*Cp_dh )
+            Constr_Alg3[nfe in 1:NFE, ncp in 1:NCP], z[3, nfe, ncp] == T_dh_ret                 + Q_whb/   ( q_dh*ρ_dh*Cp_dh) 
             #In case of more states - pattern
             #Constr_Alg999[nfe=1:NFE, ncp=1:NCP], alg[999,nfe,ncp] ==
       end)
 
       @NLconstraints(model1, begin
-            #Defining Inequality Constraints in each line
-            Constr_Ineq1[nfe in 1:1   ], u[1,nfe] - u₋₁[1]       <= 0.05
-            Constr_Ineq2[nfe in 2:NFE ], u[1,nfe] - u[1,nfe-1]   <= 0.05
+            #Defining any Inequality Constraints in each line
+            Constr_Ineq1[nfe in 1:NFE, ncp in 1:NCP], z[2, nfe, ncp] >= T_dh_minSup
             #In case of more states - pattern
             #Constr_Ineq999[nfe=1:NFE, ncp=1:NCP], alg[999,nfe,ncp] ==
       end)
 
+            #region-> std code -> Collocation Equations
+                  ## Creating a Radau collocation Matrix for NCP = 3
+                  collMat = Collocation_Matrix()
+                  @NLconstraints(model1, begin
+                        #Collocation Equation for Differential Equations
+                        #t = 0
+                        Constr_Coll_Diff0[nx in 1:Nx,  nfe = 1, ncp in 1:NCP],        x[nx, nfe, ncp]    == x0[nx]               + dt * sum(collMat[ncp, i] * dx[nx, nfe, i] for i = 1:NCP)
+                        #t = 1 ... (N-1)
+                        Constr_Coll_Diff[nx in 1:Nx,   nfe in 2:NFE, ncp = 1:NCP],    x[nx, nfe, ncp]     == x[nx, nfe-1, NCP]   + dt * sum(collMat[ncp, i] * dx[nx, nfe, i] for i = 1:NCP)
+                  end)
+                  
+            #endregion std code     
 
-            #region-> std code -> Collocation Equation
-            ## Creating a Radau collocation Matrix for NCP = 3
-            collMat = Collocation_Matrix()
-            @NLconstraints(model1, begin
-                  #Collocation Equation for Differential Equations AND Objective Function
-                  #t = 0
-                  Constr_Coll_Diff0[nx in 1:Nx,  nfe = 1, ncp in 1:NCP],        x[nx, nfe, ncp]    == x0[nx]              + dt * sum(collMat[ncp, i] * dx[nx, nfe, i] for i = 1:NCP)
-                  Constr_Coll_quad0[             nfe = 1, ncp in 1:NCP],        q[1, nfe, ncp]     == q0                  + dt * sum(collMat[ncp, i] * dq[1, nfe, i]  for i = 1:NCP)
-                  #t = 1 ... (N-1)
-                  Constr_Coll_Diff[nx in 1:Nx,   nfe in 2:NFE, ncp = 1:NCP],    x[nx, nfe, ncp]     == x[nx, nfe-1, NCP]   + dt * sum(collMat[ncp, i] * dx[nx, nfe, i] for i = 1:NCP)
-                  Constr_Coll_quad[              nfe in 2:NFE, ncp = 1:NCP],    q[1, nfe, ncp]      == q[1, nfe-1, NCP]    + dt * sum(collMat[ncp, i] * dq[1, nfe, i]  for i = 1:NCP)
-            end)
-            #endregion std code
-
+            
       #endregion
 
-      ## Solve the model
+##* Solve the model
       optimize!(model1)
       JuMP.termination_status(model1)
       JuMP.solve_time(model1::Model)
@@ -182,27 +177,65 @@ function Solve_OCP(x0, u₋₁, y_sp)
       star_u = JuMP.value.(u[:, :])
       star_z = JuMP.value.(z[:, :, NCP])
 
+      # A = JuMP.value.(z[:, :, :])
+      # B = A[1,:,:]
+      # B[1,1] = 100 
+      # T1[:,1] .= 100
+      # T1
+      # star_test
+
+      star_dx = JuMP.value.(dx[:,:,NCP])
+
+##* Plot Solution
       #region -> Plotting Solution
 
-            # t_plot = collect(T0:dt:Tf)    #Returns NFE+1 dimensional vector
-            #
-            # #choose backend for plots
+            t_plot = collect(T0:dt:Tf)    #Returns NFE+1 dimensional vector
+            
+            #choose backend for plots
             # plotly()
-            #
-            # p11 = plot(t_plot, star_x[1, :], label = "x1")
-            # p11 = plot!(t_plot, star_x[2, :], label = "x2")
-            # p11 = plot!(t_plot[1:end-1], y_sp[:], label = "y_sp",
-            #                                                             linetype = :steppost, linestyle = :dash)
-            # p12 = plot(t_plot[1:end-1], star_u[1, :], label = "u1",
-            #                                                             linetype = :steppost, marker = false)
-            # fig1 = plot(p11, p12, layout = (2, 1))
+            gr()
+
+                  #Differential States
+                  p11 = plot(t_plot, star_x[1, :],                label = "T_tes",
+                                                                                    marker = true)
+                  #Manipulated Variables
+                  p12 = plot(t_plot[1:end-1], star_u[1, :],       label = "α",
+                                                                                    marker = true, linetype = :steppost)
+                  p12 = plot!(t_plot[1:end-1], star_u[2, :],      label = "Q_phb",
+                                                                                    marker = true, linetype = :steppost)
+                  #Algebraic States
+                  p13 = plot(t_plot[2:end], star_z[1,:],          label = "T_b", 
+                                                                                    marker = true)
+                  p13 = plot!(t_plot[2:end], star_z[2,:],         label = "T_phb", 
+                                                                                    marker = true)
+                  p13 = plot!(t_plot[2:end], star_z[3,:],         label = "T_whb", 
+                                                                                    marker = true)
+
+            fig1 = plot(p11, p12, p13, layout = (3, 1))
 
       #endregion
 
 
-      return star_u[:, 1]
+#       return star_u[:, 1]
 
 
-end
+# end
 
 
+
+##*Testing some mock codes
+
+#when indexing parts of array, link is broken
+# A = [1 2 3 4 5 6]
+# B = A[1:3]
+
+# B[1]
+
+# B[1] = 10
+
+# A
+
+
+# C = ones(3,3,3)
+# D = C[1,:,:]
+# D[1,1] = 100
